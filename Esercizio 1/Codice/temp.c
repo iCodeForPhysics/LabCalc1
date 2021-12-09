e7422c0f8575e058fbe80a983399b0360eea772f
iCodeForPhysics <95830245+iCodeForPhysics@users.noreply.github.com>
2021-12-09 01:39:16 +0100

    Add files via upload

--- Esercizio 1/Codice/temp.c
+++ Esercizio 1/Codice/temp.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <stdio.h> // Includo la libreria
 
 int main() {
-    double tc, tf, conv, offset;
+    double tc, tf, conv, offset; // Dichiaro le variabili
     conv = 5./9.;
     offset = 32.;
     printf("Inserisci la temperatura in gradi Celsius \n"); 
-    scanf("%lf", &tc);
+    scanf("%lf", &tc); // Assegno a tc l'input dell'utente
     tf = tc/conv + offset;
-    printf("La temperatura in gradi Fahrenheit vale %5.2f gradi\n",tf);
+    printf("La temperatura in gradi Fahrenheit vale %5.2f gradi\n",tf); // Stampo il risultato
 }
